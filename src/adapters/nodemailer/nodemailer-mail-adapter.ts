a341255eb7238c4a34166df135329ea9182f13ba
J3remias-Barcelos <jeremiasbarceloa96@gmail.com>
2022-05-17 23:36:29 -0300

    Nlw

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b1d97e364891e5",
+      pass: "27549b446520cd"
+    }
+  });
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Jeremias Barcelos <jeremiasbarceloa96@gmai.com>',
+            subject,
+            html: body,
+        });
+    };
+}
